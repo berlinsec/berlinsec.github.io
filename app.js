34f38aecbd2d3020f514a2630fa7f8c9e25c2d33
Frederik Braun <fbraun+gh@mozilla.com>
2016-09-01 10:02:32 +0200

    nits

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function meetupCallback(result) {
 
     h1.insertAdjacentHTML('afterend',
       '<p>Check back later or join our <a ' +
-      'href="http://www.meetup.com/Berlin-Application-Security-Cryptography-Meetup">' +
+      'href="https://www.meetup.com/Berlin-Application-Security-Cryptography-Meetup">' +
       'meetup group</a></p>');
   }
 
